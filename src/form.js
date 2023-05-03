cb0cc0db9acf9121f85f05bfefddf6a2dc1577b1
Edgar Dominguez <eddomiw@gmail.com>
2023-05-02 22:36:31 -0700

    Create form to add items on to-do list

---
+++ src/form.js
@@ -0,0 +1,8 @@
+function toggleContainer() {
+  var container = document.getElementById("form-container");
+  if (container.style.display === "none") {
+    container.style.display = "block";
+  } else {
+    container.style.display = "none";
+  }
+}
